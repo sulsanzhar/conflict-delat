6136a952151832798de3e6d639db1a031847d7c4
Sultanov Sanzhar <sulsanzhar@mail.ru>
2025-07-04 19:16:30 +0500

    as

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <main>
+                <h2>Time to get started!</h2>
+            </main>
+        </div>
+    );
+}
+
+export default App;
